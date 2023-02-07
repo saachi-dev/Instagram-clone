303a710a67cccae232f129d26eb2507d5867c37c
Harsh Parashar (Nokia) <harsh.parashar@nokia.com>
2023-02-07 22:51:31 +0530

    js changes for posts

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
-// QUERY SELECTORS
+var container = document.getElementById("container")
+var ChooseFile = document.getElementById("file")
 
-var likeBtn = document.querySelector(".like-btn");
-var createPost = document.querySelector(".create-post")
-var postFooter = document.querySelector(".post-footer")
-let likeCount = document.querySelector(".like-count")
-var ChooseFile = document.querySelector(".choose-file")
-var container = document.querySelector(".container")
-// EVENT LISTENERS
+function getLike(ele) {
 
-likeBtn.addEventListener("click", getLike)
-
-//FUNCTIONS
-
-
-//LIKE LOGIC
-function getLike(e) {
-    var img = document.querySelector(".like-img")
-    if (likeBtn.classList.contains("like-btn")) {
-        img.src = './assets/heart.png'
-        likeBtn.classList.remove("like-btn")
-        likeBtn.classList.add("liked")
+    if (ele.classList.contains("like-btn")) {
+        ele.childNodes[1].src = './assets/heart.png'
+        ele.classList.remove("like-btn")
+        ele.classList.add("liked")
         likeCount.textContent++;
 
     }
     else {
-        img.src = './assets/emptyheart.png'
-        likeBtn.classList.remove("liked")
-        likeBtn.classList.add("like-btn")
+        ele.childNodes[1].src = './assets/emptyheart.png'
+        ele.classList.remove("liked")
+        ele.classList.add("like-btn")
         likeCount.textContent--;
     }
 }
@@ -34,19 +21,22 @@ function getLike(e) {
 
 
 //CREATE POST LOGIC
-var uploadedImg = ""
-var link=""
 
-ChooseFile.addEventListener("change", function() {
+ChooseFile.addEventListener("change", newPost)
+
+function newPost(e) {
+    console.log("yes")
+    var uploadedImg = ""
+    var link = ""
     const reader = new FileReader()
-    
+
     reader.addEventListener("load", () => {
         uploadedImg = reader.result
-        link=`url(${uploadedImg})`
-        console.log(link)
+        link = `url(${uploadedImg})`
+        //console.log(link)
     })
     reader.readAsDataURL(this.files[0])
-    var postEl=`<div class="post">
+    var postEl = `<div class="post" >
     <div class="post-header">
 <img class="post-dp" src="./assets/ig dp1.jpg" >
 <span class="username"><h4>saachi_dev</h4></span>
@@ -56,20 +46,15 @@ ChooseFile.addEventListener("change", function() {
 <img class="post-image" src=" ${link} " >
 </div>
 <div class="post-footer">
-<button class="like-btn"> <img class="like-img" src="./assets/emptyheart.png" ></button>
+<button class="like-btn" onclick="getLike(this)"> <img class="like-img" src="./assets/emptyheart.png" ></button>
 <button class="comment-btn"><img src="./assets/comment.png" ></button>
 <button class="share-btn"><img src="./assets/share.png"></button>
 <button class="save-btn"><img src="./assets/save.png"></button>
 </div>
 <h5> <span class="like-count">125</span> likes</h5>
 </div>`
-container.innerHTML=container.innerHTML+postEl
-
-
-})
-
-
-
-
-
+console.log("yes2")
+container.innerHTML = container.innerHTML + postEl
+document.getElementById("form").reset()
+}
 
